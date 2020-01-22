7fa1c9e0a08ef1c4dcf8cd25bb6b8bc31b8e0e5d
maxcuda <mfatica@gmail.com>
2020-01-22 12:50:26 -0500

    Input 13 and new stats.py that formats the table

--- stats.py
+++ stats.py
@@ -1,11 +1,14 @@
 import os
 import numpy as np
-
 f=os.listdir("n256")
 #print(f) 
-
 os.chdir('n256')
-
-for nm in f:
-	x=np.loadtxt(nm,usecols=(2))
-	print(nm,x.mean(),x.std(),np.median(x),x.min(),x.max())
+print("# 3SAT_results")
+print("| Input | Mean | Std | Median | Min | Max |")
+print("| ----- | -----| ---- |  ----- | ----- | ----- |")
+for nm in sorted(f, key=lambda a : int(a.split('_')[-1])):
+  x = np.loadtxt(nm, usecols=(2))
+  #print(nm,x.mean(),x.std(),np.median(x),x.min(),x.max())
+  idx = int(nm.split('_')[-1])
+  vals = [idx, x.mean(), x.std(), np.median(x), x.min(), x.max()]
+  print("| " + " | ".join([str(x) for x in vals]) + " |")
